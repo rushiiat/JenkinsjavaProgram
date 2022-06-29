97765b6f9afaa76ceae99456f4e97e29a4b80799
Rushikesh Atalkar <rushhiiatalkar@gmail.com>
2022-06-29 10:52:54 +0530

    Update calculator

--- Calculator.java
+++ Calculator.java
@@ -10,8 +10,7 @@ public class Calculator {
 		System.out.println("Enter second number : ");
 		double b = sc.nextInt();
 		
-		double d = add(a,b);
-		System.out.println("Addition is : "+d);
+		
 		
 		System.out.println("1. Addition 2. Subtraction 3. Multiplication 4. Division 5. Percentage");
 		System.out.println("Enter choice : ");
